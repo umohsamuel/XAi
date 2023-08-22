7bf4e2b15cb4794134f4dd64f8b67562c2a2a07c
Umoh Samuel Godwin <youngking074@gmail.com>
2023-08-22 22:11:12 +0100

    logos and co

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home";
+// import Planets from "./components/planets";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
       </Routes>
+      {/* <Planets /> */}
     </>
   );
 }
--- src/components/video.jsx
+++ src/components/video.jsx
@@ -1,3 +1,7 @@
 import bg1p1 from "../assets/videos/bg1p1.mp4";
+import placeholder from "../assets/videos/placeholder.mp4";
+import placeholderdown from "../assets/videos/placeholderdown.mp4";
+import bottom from "../assets/videos/bottom.png";
+import boy from "../assets/videos/boy.png";
 
-export default { bg1p1 };
+export default { bg1p1, placeholder, placeholderdown, bottom, boy };
