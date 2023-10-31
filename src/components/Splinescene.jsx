16f1dcd9453581e7b1f93c31122be8b6ab7e0e6d
Your GitHub Username <Your GitHub Email>
2023-10-31 12:57:59 +0100

    mssgs

--- src/components/Splinescene.jsx
+++ src/components/Splinescene.jsx
@@ -1,13 +1,14 @@
-import Spline from "@splinetool/react-spline";
+import Spline from '@splinetool/react-spline';
 
 
-export default function Splinescene() {
+export default function App() {
 
-    return(
+  const loc = "./../assets/3Dassets/spls.splinecode";
+  const mlink = "https://drive.google.com/file/d/1aM_dTwZs9RENGaEaZEG2tOEYiYiTQqJR/view?usp=drive_link";
+  const flink ="https://prod.spline.design/dpTC477hE8QyTukQ/scene.splinecode";
 
 
-        <Spline scene="https://prod.spline.design/dpTC477hE8QyTukQ/scene.splinecode" />
-
-    );
-
-};
+  return (
+    <Spline scene= {flink} />
+  );
+}
